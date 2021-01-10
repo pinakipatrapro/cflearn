5195a9253b0e2e4ca54218f94a514a109f4f2f8a
Pinaki Patra <pinaki.patra.pro@gmail.com>
2021-01-10 12:48:45 +0000

    hana module

--- srv/server.js
+++ srv/server.js
@@ -2,11 +2,27 @@ const express = require('express')
 const app = express()
 const SapCfAxios = require('sap-cf-axios').default;
 const xsenv = require('@sap/xsenv');
+const bodyParser = require('body-parser');
 
+xsenv.loadEnv();
+const services = xsenv.getServices({
+    hana: { tag: 'hana' },
+    uaa: { tag: 'xsuaa' }
+});
+
+
+const hdbext = require('@sap/hdbext');
+app.use(hdbext.middleware(services.hana));
+
+const xssec = require('@sap/xssec');
+const passport = require('passport');
+passport.use('JWT', new xssec.JWTStrategy(services.uaa));
+app.use(passport.initialize());
+app.use(passport.authenticate('JWT', { session: false }));
+
+app.use(bodyParser.json());
 
 app.get('/srv/Product', async function (req, res) {
-    //   res.send(JSON.stringify(req.originalUrl))
-            xsenv.loadEnv();
 
     const axios = SapCfAxios("northwind");
     var response = await axios({
@@ -26,5 +42,13 @@ app.get('/srv/Product', async function (req, res) {
     res.send([...response.data.d.results,...responseProd.data.d.results])
 
 })
-
+app.get('/srv/sales', function (req, res) {
+    req.db.exec('SELECT * FROM "nodetrial1.db::sales"', function (err, results) {
+        if (err) {
+            res.type('text/plain').status(500).send('ERROR: ' + err.toString());
+            return;
+        }
+        res.status(200).json(results);
+    });
+});
 app.listen(process.env.PORT || 5001)
